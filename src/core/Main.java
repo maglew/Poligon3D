00c6cb5dc2d19ad4894c7837356a32fc91c396a0
maglew <v.crav4uk@yandex.ru>
2019-08-30 20:12:34 +0300

    12: Specular Lighting

--- src/core/Main.java
+++ src/core/Main.java
@@ -30,7 +30,9 @@ public class Main {
         RawModel model = OBJLoader.loadObjModel("dragon",loader);
 
         TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("white")));
-
+ModelTexture texture=staticModel.getTexture();
+texture.setShineDamper(10);
+texture.setReflectivity(1);
         Entity entity = new Entity(staticModel, new Vector3f(0,-2,-20),0,0,0,1);
         Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
         Camera camera = new Camera();
